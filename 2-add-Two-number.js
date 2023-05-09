f541456383e5aef441a47e227d7cd539a4debac1
shubham <shubhampandeyup07@gmail.com>
2023-05-09 18:53:57 +0530

    number addition

--- 2-add-Two-number.js
+++ 2-add-Two-number.js
@@ -1,5 +1,16 @@
-const num1 =5;
-const num2 =4;
+//Program to Add Two Numbers
 
-const sum = num1 + num2;
-console.log(sum);
+// const num1 =50;
+// const num2 =40;
+
+// const sum = num1 + num2;
+// console.log(sum);
+
+
+function  addition (a,b){
+    return a+b;
+};
+let num1 = 20;
+let num2 = 30;
+console.log(addition(num1,num2));
+
