22e3f163b504c3af9de2e75e14f808ab55d43924
shubham <shubhampandeyup07@gmail.com>
2023-10-11 23:33:28 +0530

    change somthing in the code

--- 2-add-Two-number.js
+++ 2-add-Two-number.js
@@ -7,10 +7,24 @@
 // console.log(sum);
 
 
-function  addition (a,b){
-    return a+b;
-};
-let num1 = 20;
-let num2 = 30;
-console.log(addition(num1,num2));
+//method2
+
+// function  addition (a,b){
+//     return a+b;
+// };
+// let num1 = 20;
+// let num2 = 30;
+// console.log(addition(num1,num2));
+
+
+//method 3
+
+function add2() {
+        let sum = 0;
+        for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+        }
+        return sum;
+       }
+       console.log(add2(1, 2));
 
