2f9e1263189000dfa4f07b4dc90307614f458be2
shubham <shubhampandeyup07@gmail.com>
2023-10-16 21:24:29 +0530

    Find factorial of number using recusion

---
+++ 30-factorialNum.js
@@ -0,0 +1,14 @@
+//Program to Find Factorial of Number Using Recursion
+
+
+function factorial(x) 
+{ 
+
+  if (x === 0)
+ {
+    return 1;
+ }
+  return x * factorial(x-1);
+         
+}
+console.log(factorial(5));
