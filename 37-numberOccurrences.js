175a817b7a85ae8a09e50882ec2fd8bd69542402
shubham <shubhampandeyup07@gmail.com>
2023-10-17 08:31:54 +0530

    Number of Occurreences of a character in the String

---
+++ 37-numberOccurrences.js
@@ -0,0 +1,20 @@
+//Check the Number of Occurrences of a Character in the String
+
+function countString(str, letter){
+    let count = 0;
+    for(let i=0; i<str.length; i++){
+        //check if the character is at that position
+
+        if(str.charAt(i)=== letter){
+            count += 1;
+        }
+    }
+    return count;
+}
+const prompt = require('prompt-sync')();
+const string = prompt('Enter a string')
+const letterToCheck = prompt("Enter a letter to check:")
+
+//passting parameters and calling the function
+const result = countString(string, letterToCheck);
+console.log(result);
