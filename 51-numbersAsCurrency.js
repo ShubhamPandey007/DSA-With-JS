80d93921df1de226a3493c3f2bd12d5d71d714d6
shubham <shubhampandeyup07@gmail.com>
2023-10-30 11:49:52 +0530

    change some concept

--- 51-numbersAsCurrency.js
+++ 51-numbersAsCurrency.js
@@ -1,4 +1,21 @@
-//JavaScript Program to Format Numbers as Currency Strings
+// program to format numbers as currency string
 
-const number = 1234.5678;
-const result = ''
+// Format Numbers as Currency String Using concatenation
+
+// const number = 1234.5678;
+
+// const result = '$ ' + number.toFixed(2);
+
+// console.log(result);
+
+
+
+
+
+// Format Numbers as Currency String Using toLocaleString()
+const result =  (2500).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+console.log(result);
