1114bc7ecf26006789a18fd4f0ff30b8da6d6ecc
shubham <shubhampandeyup07@gmail.com>
2023-11-03 12:28:55 +0530

    replace all istances of character in a string

---
+++ 58-replaceAll-Instance.js
@@ -0,0 +1,12 @@
+// program to replace all instances of a character in a string
+
+const string = 'Learning JavaScript Program';
+
+const result = string.replace(/a/g, "A");
+
+console.log(result);
+
+
+const words = 'learnign is working with system';
+const result1 = string.replace(/e/g, 'E');
+console.log(result1);
