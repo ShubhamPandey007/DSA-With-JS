b062765cd59ac84d9147862d82728fa8976eb693
shubham <shubhampandeyup07@gmail.com>
2024-01-02 00:17:22 +0530

    empty an array

--- 71-emptyArray.js
+++ 71-emptyArray.js
@@ -3,10 +3,31 @@
 
 // program to empty an array
 
+// function emptyArray(arr) {
+
+//     // substituting new array
+//     arr = [];
+    
+//     return arr;
+// }
+
+// const array = [1, 2 ,3];
+// console.log(array);
+
+// // call the function
+// const result = emptyArray(array);
+// console.log(result);
+
+
+
+
+
+// program to empty an array
+
 function emptyArray(arr) {
 
-    // substituting new array
-    arr = [];
+    // setting array length to 0
+    arr.length = 0;
     
     return arr;
 }
