6cda08e2168a08a78ff7f7ab3c3e3c8f54c49935
shubham <shubhampandeyup07@gmail.com>
2024-01-03 01:12:35 +0530

    add element to Array using splice

--- 72-addArray.js
+++ 72-addArray.js
@@ -1,16 +1,33 @@
 // program to add element to an array
-const array = [1, 2, 3];
+// const array = [1, 2, 3];
+
+// function addElement(arr) {
+
+//     // adding new array element
+//     arr.unshift(4);
+//     arr.push(6);
+//     arr.shift();
+    
+//     console.log(arr);
+// }
+
+// calling the function
+// passing array argument
+// addElement(array);
+
+//Add Element to Array Using splice()
+
+// program to add element to an array
 
 function addElement(arr) {
 
-    // adding new array element
-    arr.unshift(4);
-    arr.push(6);
-    arr.shift();
+    // adding element to array
+    arr.splice(0, 0, 4);
     
     console.log(arr);
 }
 
+const array = [1, 2, 3];
+
 // calling the function
-// passing array argument
 addElement(array);
