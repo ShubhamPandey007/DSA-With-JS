3d7058ffadab6c2a76773c173d94721f4bb89303
shubham <shubhampandeyup07@gmail.com>
2024-01-04 01:35:00 +0530

    udate

--- 74-mergeTwoArrays.js
+++ 74-mergeTwoArrays.js
@@ -23,3 +23,25 @@ const array2 = [2, 3, 5]
 // calling the function
 // passing array argument
 getUniqueAfterMerge(array1, array2);
+
+
+//Using Spread Syntax and Set
+
+// program to merge and remove duplicate value from an array
+
+function getUniqueAfteMerge(arr1, arr2){
+
+    // merge two arrays
+    let arr = [...arr1, ...arr2];
+
+    // removing duplicate
+    let uniqueArr = [...new Set(arr)];
+
+    console.log(uniqueArr);
+}
+
+const arr1 = [1, 2, 3];
+const arr2 = [2, 3, 5]
+
+// calling the function
+getUniqueAfteMerge(array1, array2);
